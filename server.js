197b2f01efb81c107c9c91c90284fdc86d73b5e0
NewJesu$ <68636130+NewJ4sus@users.noreply.github.com>
2025-03-23 19:34:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = 3456;
 
-const url_client = 'https://client-camera-food.vercel.app/';
+const url_client = 'https://client-camera-food.vercel.app';
 
 // Настройки CORS
 const corsOptions = {
